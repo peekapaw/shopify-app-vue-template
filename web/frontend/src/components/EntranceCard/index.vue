a04afa1f678af2ac78eb45be61da3b33a34e8c82
wellen zhong <wellen2000@qq.com>
2024-03-24 14:09:53 +0000

    feat: homepage

---
+++ web/frontend/src/components/EntranceCard/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="w-full inline-block cursor-pointer rounded-md p-2 hover:bg-gray-100">
+    <div>
+      <div class="flex">
+        <div class="flex-none w-20 h-20 relative">
+          <div class="w-12 h-12 mt-0 pt-1 text-center rounded-md absolute top-0 right-0 bottom-0 left-0 bg-gray-200">
+            <slot name="icon"></slot>
+          </div>
+        </div>
+        <div class="flex-1">
+          <div class="text-primary text-sm font-bold">{{ title }}</div>
+          <div v-if="description">{{ description }}</div>
+          <div v-else><slot></slot></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: String,
+  description: String
+})
+</script>
+
+<style lang="scss" scoped></style>
--- web/frontend/src/views/Home/index.vue
+++ web/frontend/src/views/Home/index.vue
@@ -1,25 +1,88 @@
 <template>
-    <div class="max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg mx-auto mt-4">
+  <div class="max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg mx-auto mt-4">
+    <v-card class="mx-auto" color="indigo" prepend-icon="mdi-information">
+      <template v-slot:title> Welcome! </template>
+      <v-card-text>
+        {{ bannerText }}
+      </v-card-text>
+    </v-card>
+    <v-card class="mt-4">
+      <v-row no-gutters>
+        <v-col sm="12" md="6" lg="4" xl="4">
+          <v-sheet class="ma-2 pa-2">
+            <EntranceCard title="Discount roups">
+              <template #icon>
+                <v-icon size="36">mdi-account-multiple</v-icon>
+              </template>
 
-        <v-card class="mx-auto" color="indigo" prepend-icon="mdi-information">
+              Customer groups to offer wholesale prices & discounts.</EntranceCard
+            ></v-sheet
+          >
+        </v-col>
+        <v-col sm="12" md="6" lg="4" xl="4">
+          <v-sheet class="ma-2 pa-2">
+            <EntranceCard title="Percentage discounts">
+              <template #icon>
+                <v-icon size="36">mdi-label-percent-outline</v-icon>
+              </template>
 
-            <template v-slot:title>
-                Welcome!
-            </template>
-            <v-card-text>
-                {{ bannerText }}
-            </v-card-text>
-        </v-card>
-        <v-card class="mt-4">
-            <v-card-text>hellovbtn</v-card-text>
-        </v-card>
-        
+              Offer percentage discounts specific to a customer tag.</EntranceCard
+            ></v-sheet
+          >
+        </v-col>
+        <v-col sm="12" md="6" lg="4" xl="4">
+          <v-sheet class="ma-2 pa-2">
+            <EntranceCard title="Individaual variant prices">
+              <template #icon>
+                <v-icon size="36">mdi-tag-multiple-outline</v-icon>
+              </template>
 
-    </div>
+              Offer specific prices like $16.28 or $72 for any customer.</EntranceCard
+            ></v-sheet
+          >
+        </v-col>
+        <v-col sm="12" md="6" lg="4" xl="4">
+          <v-sheet class="ma-2 pa-2">
+            <EntranceCard title="Individaual variant prices">
+              <template #icon>
+                <v-icon size="36">mdi-tag-multiple-outline</v-icon>
+              </template>
+
+              Offer specific prices like $16.28 or $72 for any customer.</EntranceCard
+            ></v-sheet
+          >
+        </v-col>
+        <v-col sm="12" md="6" lg="4" xl="4">
+          <v-sheet class="ma-2 pa-2">
+            <EntranceCard title="Individaual variant prices">
+              <template #icon>
+                <v-icon size="36">mdi-tag-multiple-outline</v-icon>
+              </template>
+
+              Offer specific prices like $16.28 or $72 for any customer.</EntranceCard
+            ></v-sheet
+          >
+        </v-col>
+        <v-col sm="12" md="6" lg="4" xl="4">
+          <v-sheet class="ma-2 pa-2">
+            <EntranceCard title="Individaual variant prices">
+              <template #icon>
+                <v-icon size="36">mdi-tag-multiple-outline</v-icon>
+              </template>
+
+              Offer specific prices like $16.28 or $72 for any customer.</EntranceCard
+            ></v-sheet
+          >
+        </v-col>
+      </v-row>
+    </v-card>
+  </div>
 </template>
 
 <script setup>
-const bannerText = 'As our teammates celebrate the color festival, support may be slightly slower than usual. We appreciate your understanding and are grateful for your continued support. Happy Holi!'
+import EntranceCard from '@/components/EntranceCard/index.vue'
+const bannerText =
+  'As our teammates celebrate the color festival, support may be slightly slower than usual. We appreciate your understanding and are grateful for your continued support. Happy Holi!'
 </script>
 
 <style lang="scss" scoped></style>
